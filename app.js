de8d90f449baba4b35701374956f82c22d53e0c4
Fay <samsongreen72@gmail.com>
2022-05-06 13:29:55 -0400

    Added function to display and update the stitch count when adjusted up or down.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+const stitchCountH2 = document.getElementById('stitch-count');
 let count = 0;
+
+function displayCount() {
+  stitchCountH2.innerHTML = count;
+}
 
 function updateCount(event) {
   if (event.target.id === "up-btn") {
     count++;
+    displayCount()
   } else {
     count--;
+    displayCount()
   }
   console.log(count);
 }
